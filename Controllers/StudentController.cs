fa99d8cbaca6518ad50bd40f888890866509b496
SH-PKD-007 <SH-PKD-007@DESKTOP-OR85DOS>
2022-01-30 11:17:00 -0800

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using ManagementSystem.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReadData(string name1,string subject,string email)
+        {
+            Helper mail = new Helper();
+            mail.FileWrite(name1, subject, email);
+
+            return View();
+        }
+    }
+}
---
+++ Utils/Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace ManagementSystem.Utils
+{
+    public class Helper
+    {
+        public void FileWrite(string name,string subject,string email)
+        {
+
+
+            StreamWriter st = new StreamWriter(@"C:\Users\SH-PKD-007\source\repos\ManagementSystem\staicFiles\" + name+ ".txt");
+            st.WriteLine("Employe Name:" + name);
+            st.WriteLine("Employe Subject:" + subject);
+            st.WriteLine("Employe Salary:" + email);
+         
+            st.Close();
+        }
+
+    }
+}
